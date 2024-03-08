c05417a0f2919436488d8a048bb929906d7bddb0
aidansterling6 <aidansterling6@gmail.com>
2024-03-08 13:25:55 -0800

    update

--- js/sketch.js
+++ js/sketch.js
@@ -25,24 +25,31 @@ function setup() {
   });
   //Draw the loaded image onto the canvas
   image(img, 0, 0, width, height);
-  var f1 = new Food("bacon", 200, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f2 = new Food("cheese", 260,1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f3 = new Food("dough", 320, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f4 = new Food("cheesy-dough", 380, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f5 = new Food("cheesier-dough", 440, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f6 = new Food("goop", 500, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f7 = new Food("mushroom", 560, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f8 = new Food("pepperoni", 620, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f9 = new Food("pizza", 680, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f10 = new Food("pizza-dough", 740, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f12 = new Food("tomato", 800, 1200, 30, FOODLIB, [], "icon", "ingredients");
-  var f13 = new Food("blender", 1464, 816, 45, FOODLIB, [], "tool", "tools");
-  var f14 = new Food("knife", 1464, 1020, 30, FOODLIB, [], "tool", "tools");
-  var f15 = new Food("oven", 560, 400, 60, FOODLIB, [], "tool", "tools");
+  Food.setBase(0, 0, width, height);
+  var f1 = new Food("bacon", 200, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f2 = new Food("cheese", 260,150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f3 = new Food("dough", 320, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f4 = new Food("cheesy-dough", 380, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f5 = new Food("cheesier-dough", 440, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f6 = new Food("goop", 500, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f7 = new Food("mushroom", 560, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f8 = new Food("pepperoni", 620, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f9 = new Food("pizza", 680, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f10 = new Food("pizza-dough", 740, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+  var f12 = new Food("tomato", 800, 150, 30, FOODLIB, [], "icon", "ingredients", 0, 0, width, height);
+
+
+  var f10 = new Food("pizza-dough", 740, 150, 30, FOODLIB, [], "food", "ingredients", 0, 0, width, height);
+  
+  var f13 = new Food("blender", 0.4, 0.4, 45, FOODLIB, [], "tool", "tools", 0, 0, width, height);
+  var f14 = new Food("knife", 0.5, 0.4, 30, FOODLIB, [], "tool", "tools", 0, 0, width, height);
+  var f15 = new Food("oven", 0.6, 0.4, 60, FOODLIB, [], "tool", "tools", 0, 0, width, height);
 }
 
 function draw() {
+  imageMode(CORNER);
   image(img, 0, 0, width, height);
-  Food.drag();
-  Food.draw();
+  imageMode(CENTER);
+  Food.drag(0, 0, width, height);
+  Food.draw(0, 0, width, height);
 }
