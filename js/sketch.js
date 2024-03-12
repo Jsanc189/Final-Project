322e8da55bedf71a33f638ab093f47a25907141a
Jackie Sanchez <jsanc189@ucsc.edu>
2024-03-12 13:46:55 -0700

    updatedsketch to have a tracery generator
    Co-authored-by: Lyssa Li <maozblan@users.noreply.github.com>
    Co-authored-by: wiwechen <wiwechen@users.noreply.github.com>

--- js/sketch.js
+++ js/sketch.js
@@ -7,6 +7,9 @@ let blender;
 let knife;
 let trash;
 let pres_table;
+// let yes = true
+
+let grammar; //stores the text for reviews of food
 
 let FOODLIB;
 
@@ -27,6 +30,7 @@ function preload() {
   console.log(FOODLIB);
 
   Food.LoadImages(FOODLIB);
+
 }
 
 function setup() {
@@ -98,5 +102,58 @@ function draw() {
   Food.drag();
   Food.draw();
 
+  // if(yes == true){
+  //   for(let i = 0; i < 4; i++){ 
+  //     loadGeneratedText("pizza", ["cheese", "goop", "pepperoni", "mushroom"]);
+  //   }
+  //   yes = false;
+  // }
 
+}
+
+function loadGeneratedText(food, toppings) {
+  let randomIndex = Math.floor(Math.random() * toppings.length);
+  let topping1 = toppings[randomIndex];
+  randomIndex = Math.floor(Math.random() * toppings.length);
+  let topping2 = toppings[randomIndex];
+  var words = 
+    {
+        origin:{
+            "rating": ["5 OUT OF 5 STARS!  #positive_event#.", 
+                      "4 out of 5 stars.  #positive_event#.", 
+                      "3 out of 5 stars.  #neutral_event#.",
+                      "2 out of 5 stars.  #neutral_event#", 
+                      "1 out of 5 stars.  #negative_event#.", 
+                      "0 OUT OF 5 STARS!  #negative_event#" 
+                     ],
+            "positive_event": ["Enjoy a cozy Italian restaurant ambiance. #food# was topped with fresh #topping1#.  What a delight!",
+                               "Picture a bustling pizzeria where skilled chefs craft pizzas in a roaring wood fired oven. The #food# was just premium!",
+                               "Step into a trendy pizzeria with colorful, stunning pizzas!  Each bite of #food# tastes handcrafted with care and devotion.  Loved the fresh #topping1#!",
+                               "Experience the classic taste of Naples with this #food# topped with #topping1# and #topping2#.  It's a taste of tradition, simple yet sublime.",
+                               "From farm to table!  This place sources all its #topping1# and #topping2# from local farms.  Its a taste of the countrside with the #food#!" 
+                            ],
+            "neutral_event":  ["Nothing to special. The famed #topping1# was not up to the hype others built.",
+                              "The #food# was kinda mid, nothing impressive",
+                              "The #food# wasn't bad, but I've had better at different pizzaera",
+                              "The #food# was good, but I think the price was a bit too high than what it's worth",
+                              "A not too expensive place to get a #food# if you don't want to pay too much for one. Kinda stale, but not bad to where you'll regret buying it"
+                            ],
+            "negative_event": ["Taking a bite of what appears to be a normal #food#, you're met with the unexpected texture of #topping1#. The shock was too much!",
+                               "THIS PLACE IS THE WORST!  BURNDE MY ORDER! RUD CUSTOMER SERVICE! WILL NOT BE COMING BACK!!!!",
+                               "After indulging in what seemed like a casual pizza dinner at a local joint, you wake up in the middle of the night with food poisoing!  NEVER COMING BACK!",
+                               "Soggy #food#.  The #topping1# was not fresh.  Won't be coming back....",
+                               "Sad, limp crust on #food#.   It felt like it was frozen for a month before they microwaved and served it to me.  Gross."
+                            ],
+            "food":[food],
+            "topping1":[topping1],
+            "topping2":[topping2]
+        },
+    };
+
+    var traceryGrammar = tracery.createGrammar(words.origin);
+    generatedText = traceryGrammar.flatten('#rating#');
+    console.log("Generated Text:", generatedText);
+    console.log("finished loading!");
+
+    return(generatedText);
 }
